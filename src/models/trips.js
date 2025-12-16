5de43379c2e4df27c894ca0fa64fde3fafb5db97
NodeNomad101 <dbhattarai387@gmail.com>
2025-12-16 13:07:58 +0545

    completed trip APIs

--- src/models/trips.js
+++ src/models/trips.js
@@ -1,75 +1,77 @@
-import { Schema, model } from "mongoose";   
+import { Schema, model } from "mongoose";
 
-const ExpenseSchema = new Schema( // Define the Budget schema
-  {
-    name : {
-        type: String,
-        required: true,
-    },
+const ExpenseSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+});
 
-    amount : {
-        type: Number,
-        required: true,
-    },
-    date : {
-        type: Date,
-        required: true,
-    },
-    });
+const BudgetSchema = new Schema({
+  total: {
+    type: Number,
+    required: true,
+  },
+  spent: {
+    type: Number,
+    default: 0,
+  },
+  expenses: [ExpenseSchema],
+});
 
-const BudgetSchema = new Schema({ // Define the Budget schema
-    total : {
-        type: Number,
-        required: true,
-    },
-    
-    spent: {
-        type: Number,
-        default: 0,
-    },
-    expenses : [ExpenseSchema], // Array of Expense subdocuments
-    });
-
-const TripSchema = new Schema( // Define the Trip schema
-  {
-    user : {
-      type: Schema.Types.ObjectId, 
-      ref: "User",
-      required: true,
-    },
-
-    title : {
+const TripSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  destinations: [
+    {
       type: String,
       required: true,
       trim: true,
     },
+  ],
+  budget: BudgetSchema,
+});
 
-    description : {
-        type: String,
-    },
-    
-    startDate : {
-        type: Date,
-        required: true,
-    },
-
-    endDate : {
-        type: Date,
-        required: true,
-    },
-
-    destinations : [ // Array of destination strings
-        {
-            type: String,
-            required: true,
-            trim: true,
-        },
-    ],
-    budget : {
-        type: BudgetSchema, // Embedded Budget document
-    },
+TripSchema.pre("findOneAndUpdate", function () {
+  if (this.getUpdate().budget) {
+    this.getUpdate().budget.spent +=
+      this.getUpdate().budget?.expenses?.reduce(
+        (acc, expense) => acc + expense.amount,
+        0
+      ) || 0;
+    this.getUpdate().budget.expenses?.map((expense) => {
+      expense.date = new Date();
     });
+  }
+});
 
-const Trip = model("Trip", TripSchema); // Create the Trip model
+const Trip = model("Trip", TripSchema);
 
 export default Trip;
